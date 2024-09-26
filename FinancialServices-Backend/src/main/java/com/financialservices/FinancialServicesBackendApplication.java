ecedc1937c033991eb7a04410246325228f9e78f
Samruddhi362 <82897864+Samruddhi362@users.noreply.github.com>
2024-09-26 12:06:27 +0530

    Backend - Restful apis

---
+++ FinancialServices-Backend/src/main/java/com/financialservices/FinancialServicesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.financialservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialServicesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialServicesBackendApplication.class, args);
+	}
+
+}
---
+++ FinancialServices-Backend/src/test/java/com/financialservices/FinancialServicesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialServicesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
